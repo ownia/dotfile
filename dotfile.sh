e2dbce1eb62b3e7907f7c377857b0715c39704d1
Weizhao Ouyang <o451686892@gmail.com>
2024-05-27 23:21:02 +0800

    dotfile.sh: add auto config for vim and tmux
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- dotfile.sh
+++ dotfile.sh
@@ -3,11 +3,27 @@
 usage() {
     cat 1>&2 <<EOF
 dotfile:
+      config - update configs (vim, tmux)
 EOF
 }
 
+config() {
+    echo "update .vimrc"
+    if cp configs/.vimrc ~/.vimrc; then
+        echo "done"
+    fi
+    echo "update .tmux.conf"
+    if cp configs/.tmux.conf ~/.tmux.conf; then
+        echo "done"
+    fi
+}
+
 main() {
-    usage
+    if [ "$1" = "config" ]; then
+        config
+    else
+        usage
+    fi
 }
 
 main "$@" || exit 1
