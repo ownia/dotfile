16052e0d06ca0cf7983ba2ab2215e0b80040153c
Weizhao Ouyang <weizhao.ouyang@arm.com>
2024-10-09 14:22:51 +0800

    scripts/minicom: rename default minirc
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- scripts/minicom/minicom.sh
+++ scripts/minicom/minicom.sh
@@ -1,1 +1,7 @@
-minicom --color=on --capturefile=minicom.log minirc.rk
+#!/bin/bash
+
+if [ "$#" == 1 ]; then
+	minicom --color=on --capturefile=minicom-"$1".log "$1"
+else
+	minicom --color=on --capturefile=minicom.log minirc.default
+fi
