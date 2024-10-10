49a6e9256524c741fe8fbedbe99ef468b25ddf4c
Weizhao Ouyang <weizhao.ouyang@arm.com>
2024-10-10 10:48:16 +0800

    build/arm64: update qemu script
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- build/arm64/qemu_start.sh
+++ build/arm64/qemu_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #qemu-system-aarch64 \
-/home/weiouy01/code/qemu/build/qemu-system-aarch64 \
+/data_sda/qemu/build/qemu-system-aarch64 \
 	-machine virt -cpu max \
 	-nographic -smp 4 -m 4096 \
-	-bios "/home/weiouy01/code/u-boot/u-boot.bin" \
 	-kernel arch/arm64/boot/Image \
-	-append "rw console=ttyAMA0 kgdboc=tty0,115200" \
-	-initrd "/home/weiouy01/code/rootfs.cpio" \
+	-append "rootwait root=/dev/vda" \
+	-drive file=rootfs.ext4,if=none,format=raw,id=hd0 \
+	-device virtio-blk-device,drive=hd0 \
 	-device virtio-9p-device,id=fs0,fsdev=fsdev0,mount_tag=hostshare \
 	-fsdev local,security_model=passthrough,id=fsdev0,path=./tmp \
 	#-bios "/home/weiouy01/code/RELEASEAARCH64_QEMU_EFI.fd" \
