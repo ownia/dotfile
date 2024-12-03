670b141a9d7f0d8afde7778da894448eaf62c579
Weizhao Ouyang <weizhao.ouyang@arm.com>
2024-12-03 13:52:39 +0800

    build: clean up build/
    Signed-off-by: Weizhao Ouyang <weizhao.ouyang@arm.com>

--- build/arm64/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export ARCH=arm64
-export CROSS_COMPILE=aarch64-linux-gnu-
-
-# config
-#make defconfig
-# build
-make -j"$(nproc)"
--- build/mips/qemu_start.sh
+++
@@ -1,1 +0,0 @@
-qemu-system-mips64el -cpu I6400 -M malta -smp 8 -kernel vmlinux -m 2048 -nographic --append "rw console=tty0 root=/dev/ram loglevel=8"
