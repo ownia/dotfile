df4d4614c9c6a2245047e7cb1bbecd4fc24fe302
Weizhao Ouyang <o451686892@gmail.com>
2025-03-18 17:19:12 +0800

    build/ubuntu: update version to plucky
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

--- build/ubuntu/kernel.sh
+++ build/ubuntu/kernel.sh
@@ -1,7 +1,20 @@
-build() {
-    rm *.deb
+#!/bin/sh
 
-    cd noble/
+# https://canonical-kteam-docs.readthedocs-hosted.com/en/public/how-to/build-kernel.html
+# https://wiki.ubuntu.com/Kernel/BuildYourOwnKernel
+
+# Ubuntu 24.04 Noble Numbat
+#UBUNTU_VERSION="noble"
+#UBUNTU_KERNEL_VERSION="6.8.12"
+
+# Ubuntu 25.04 Plucky Puffin
+UBUNTU_VERSION="plucky"
+UBUNTU_KERNEL_VERSION="6.14.0"
+
+build() {
+    rm ./*.deb
+
+    cd $UBUNTU_VERSION/ || exit
 
     # slow steps
     #fakeroot debian/rules clean
@@ -10,13 +23,13 @@ build() {
     # fast steps
     export CC="ccache gcc"
     export CXX="ccache g++"
-    make CC="ccache gcc" -j $(nproc --all) bindeb-pkg
+    make CC="ccache gcc" -j "$(nproc --all)" bindeb-pkg
 
     cd ../
 }
 
 reboot() {
-    sudo dpkg -i linux*6.8.12*.deb
+    sudo dpkg -i linux*$UBUNTU_KERNEL_VERSION*.deb
     sudo systemctl reboot
 }
 
